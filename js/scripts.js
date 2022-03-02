1d86ab1b640395a5724a8c661a5c6d6cc6a1083b
Juan Sebastián González López <juangonzalezlopez241829@gmail.com>
2022-03-02 15:23:03 +0100

    forth

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,16 @@ let channel = pusher.subscribe('chatApp');
 channel.bind('chatApp-event', function(data) {
     const d = new Date();
     let time = d.toLocaleTimeString();
+    let nombreE = document.getElementById('usuario').value;
     let nombre=(data["usuario"]);
     let mensaje=(data["mensaje"]);
-    $('.card-body').append('<li>'+nombre+'</li>');
-    $('.card-body').append('<small>'+mensaje+'</small><small style="float: right; color: #639ae7">'+time+'</small>');
+    if (nombreE !== nombre){
+        $('.card-body').append('<li style="color: #e75e35">'+nombre+'</li>');
+        $('.card-body').append('<small style="color: #e75e35">'+mensaje+'</small><small style="float: right; color: #639ae7">'+time+'</small>');
+    }else {
+        $('.card-body').append('<li style="color: #8648c7">'+nombre+'</li>');
+        $('.card-body').append('<small style="color: #8648c7">'+mensaje+'</small><small style="float: right; color: #639ae7">'+time+'</small>');
+    }
     document.getElementById('mensaje').value='';
 });
 function enviarDatos()
